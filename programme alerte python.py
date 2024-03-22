b1e71ee1a4bfa9d7e7c041b2d2ea209d285b3b47
RG4531 <remi.guillaume45380@gmail.com>
2024-03-22 13:32:22 +0100

    Ajout de l'alerte validée + rouge

--- programme alerte python.py
+++ programme alerte python.py
@@ -10,40 +10,44 @@ token = "tOQv4Ctp06BEAFTA9pnNOjjyKL8h4yb5nT2Ylam-blD2MNdBtRtqrOOA5bagU_GmUlgwveL
 org = "Aquapotager"
 
 # Seuil à surveiller
-seuil = 20
+Seuilmin = [0,10, 10 ]
+Seuilmax=[25, 50, 100]
+list=["temp","hygro","lux"]
 
 # Fonction pour récupérer les données
 
 def recup_donnee (client):
-    list=["temp","hygro","lux"]
+    Valeur=[[],[],[]]
+    i=0
     for valeurlist in list:
         query = f'from(bucket: "Value")\
-        |> range(start: -1d)\
+        |> range(start: -2h)\
         |> filter(fn:(r)=>r._measurement=="ENSAT" and r._field=="{valeurlist}" and r.loc=="ensat")'
         tables = client.query_api().query(query, org=org)
-        Valeur=[]
         for table in tables:
-            print(table)
+            valeur_list=[]
             for record in table.records:
-                Valeur.append(record['_value'])
-                print(record)
-                print(Valeur[-1])
-        return Valeur
+                valeur_list.append(record['_value'])
+            Valeur[i]=valeur_list
+            i+=1
+                #print(record)
+    print(Valeur)
+    return Valeur
 
 
 # Fonction pour afficher une fenêtre d'alerte
-def affichagealerte(value, threshold):
+def affichagealerte(valeurlist, value ):
     fenetre = tk.Toplevel()
     fenetre.title("Alerte!")
-    alert_label = tk.Label(fenetre, text=f"La valeur {value} dépasse le seuil de {threshold}.", font=("Arial", 50), fg="red")
+    alert_label = tk.Label(fenetre, text=f"{valeurlist} : La valeur {value} dépasse le seuil.", font=("Arial", 40), fg="red")
     alert_label.pack(padx=400, pady=300)
     dismiss_button = tk.Button(fenetre, text="Fermer", command=fenetre.destroy)
     dismiss_button.pack(pady=10)
 
 
 # Fonction pour alerter si le seuil est dépassé
-def Seuil(value):
-    if value[-1] > seuil :
+def Seuil(value,seuilmin, seuilmax):
+    if value[-1] < seuilmin or value[-1] > seuilmax :
         return True
     else:
         return False
@@ -53,18 +57,20 @@ def main():
     client = InfluxDBClient(url=url, token=token)
     while True:
         print("Connect")
-        value = recup_donnee(client)
-        if Seuil(value) :
-            print(f"[{datetime.now()}] Alerte : La valeur {value} dépasse le seuil de {seuil}")
-            root = tk.Tk()
-            affichagealerte(value[-1], seuil)
-            root.withdraw()# Cacher la fenêtre principale
-            root.after(10000, main)
-            root.mainloop()
-    time.sleep(300)
+        liste_valeur = recup_donnee(client)
+        for i in range(0,3):
+            value=liste_valeur[i]
+            seuilmin=Seuilmin[i]
+            seuilmax=Seuilmax[i]
+            valeurlist=list[i]
+            if Seuil(value,seuilmin, seuilmax) :
+                root = tk.Tk()
+                affichagealerte(valeurlist, value[-1])
+                root.withdraw()# Cacher la fenêtre principale
+                root.after(10000, main)
+                root.mainloop()
 
 
 
 if __name__ == "__main__":
     main()
-
